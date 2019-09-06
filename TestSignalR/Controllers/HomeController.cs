a5ab651dc397810d3f2c2f27c14f5edbd64eb8c7
fuzy <fxy6781349@qq.com>
2019-09-06 15:47:24 +0800

    init

---
+++ TestSignalR/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TestSignalR.Hubs;
+using TestSignalR.Models;
+
+namespace TestSignalR.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.ClientName = "聊客-" + Guid.NewGuid().ToString("N");
+            var onLineUserList = ChatHub.OnLineUsers.Select(u => new SelectListItem() { Text = u.Value, Value = u.Key }).ToList();
+            onLineUserList.Insert(0, new SelectListItem() { Text = "-所有人-", Value = "" });
+            ViewBag.OnLineUsers = onLineUserList;
+            return View();
+        }
+
+
+    }
+}
---
+++ TestSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Web;
+using Microsoft.AspNetCore.SignalR;
+
+namespace TestSignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public static ConcurrentDictionary<string, string> OnLineUsers = new ConcurrentDictionary<string, string>();
+
+        public void Send(string message)
+        {
+            string clientName = OnLineUsers[Context.ConnectionId];
+            message = HttpUtility.HtmlEncode(message).Replace("\r\n", "<br/>").Replace("\n", "<br/>");
+            Clients.All.SendAsync("receiveMessage", DateTime.Now.ToString("yyyy -MM-dd HH:mm:ss"), clientName, message);
+        }
+
+        public void SendOne(string toUserId, string message)
+        {
+            string clientName = OnLineUsers[Context.ConnectionId];
+            message = HttpUtility.HtmlEncode(message).Replace("\r\n", "<br/>").Replace("\n", "<br/>");
+            Clients.Caller.SendAsync("receiveMessage",DateTime.Now.ToString("yyyy -MM-dd HH:mm:ss"), string.Format("您对 {1}", clientName, OnLineUsers[toUserId]), message);
+            Clients.Client(toUserId).SendAsync("receiveMessage",DateTime.Now.ToString("yyyy -MM-dd HH:mm:ss"), string.Format("{0} 对您", clientName), message);
+        }
+
+        public override System.Threading.Tasks.Task OnConnectedAsync()
+        {
+            string clientName = Context.ConnectionId;
+            OnLineUsers.AddOrUpdate(Context.ConnectionId, clientName, (key, value) => clientName);
+            Clients.All.SendAsync("userChange", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), string.Format("{0} 加入了。", clientName), OnLineUsers.ToArray());
+            return base.OnConnectedAsync();
+        }
+
+        public override System.Threading.Tasks.Task OnDisconnectedAsync(Exception exception)
+        {
+            string clientName = Context.ConnectionId;
+            Clients.All.SendAsync("userChange", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), string.Format("{0} 离开了。", clientName), OnLineUsers.ToArray());
+            OnLineUsers.TryRemove(Context.ConnectionId, out clientName);
+            return base.OnDisconnectedAsync(exception);
+        }
+
+    }
+}
